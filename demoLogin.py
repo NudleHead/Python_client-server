fe89196fa7ce3bd29c7ca592eb3c64c2b1876a17
NudleHead <malpiszon001@gmail.com>
2020-03-14 14:14:35 +0100

    Python_PyQt5/client-server_commit_1

---
+++ demoLogin.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'demoLogin.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(638, 501)
+        self.labelLogin = QtWidgets.QLabel(Dialog)
+        self.labelLogin.setEnabled(True)
+        self.labelLogin.setGeometry(QtCore.QRect(100, 170, 111, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.labelLogin.setFont(font)
+        self.labelLogin.setObjectName("labelLogin")
+        self.labelPassword = QtWidgets.QLabel(Dialog)
+        self.labelPassword.setEnabled(True)
+        self.labelPassword.setGeometry(QtCore.QRect(100, 240, 101, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.labelPassword.setFont(font)
+        self.labelPassword.setObjectName("labelPassword")
+        self.lineEditLogin = QtWidgets.QLineEdit(Dialog)
+        self.lineEditLogin.setGeometry(QtCore.QRect(210, 180, 151, 31))
+        self.lineEditLogin.setObjectName("lineEditLogin")
+        self.lineEditPassword = QtWidgets.QLineEdit(Dialog)
+        self.lineEditPassword.setGeometry(QtCore.QRect(210, 250, 151, 31))
+        self.lineEditPassword.setFrame(True)
+        self.lineEditPassword.setObjectName("lineEditPassword")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(390, 220, 171, 61))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.labelLogin.setText(_translate("Dialog", "Login:"))
+        self.labelPassword.setText(_translate("Dialog", "Password:"))
+        self.pushButton.setText(_translate("Dialog", "Sign in"))
---
+++ serverr.py
@@ -0,0 +1,47 @@
+import socket
+import pickle
+from _thread import *
+import sys
+
+server = "192.168.8.104"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+answer = ["Ola", "spi"]
+
+
+def threaded_client(conn):
+    conn.send(pickle.dumps("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = pickle.loads(conn.recv(2048))
+            reply = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+            # conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn,))
